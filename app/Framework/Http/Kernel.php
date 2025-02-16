fd7ccd7a2fceb7fcee76e495e2d3e26c282517d4
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-16 00:55:49 -0300

    feat: use Reflection on the Kernel to allow similar behaviour to Laravel's parameter injection into controller methods

--- app/Framework/Http/Kernel.php
+++ app/Framework/Http/Kernel.php
@@ -32,20 +32,54 @@ class Kernel
     private function callHandler(mixed $handler, array $params, Request $request)
     {
         // Reflection API: used to read the arguments a function has specified and using that to decide which params should be passed
-        // pretty much replicates Laravel's Service Provider pattern
-        // (W.I.P) for now just throw an error if the param count was incorrect
-        try {
-            $result = call_user_func_array($handler, [$request, ...$params]);
-            return new Response($result);
-        } catch (\ArgumentCountError $e) {
-            return new Response(
-                content: '
-                    <h3>Ocorreu um erro crítico, contate seu administrador.</h3>
-                    <b>Descrição:</b> The controller method to this route was not built to receive the route parameters and the request.
-                ',
-                status: 500,
-            );
+        // pretty much replicates Laravel's parameter injection structure (laravel's is obviously more polished)
+
+        if (is_array($handler) && count($handler) === 2) {
+            // The handler is probably a class method, since it's probably in the format [class_name, method_name]
+            $reflection = new \ReflectionMethod($handler[0], $handler[1]);
+        } else if (is_callable($handler)) {
+            // The handler is callable and is not an array, so it is probably a closure or a function
+            $reflection = new \ReflectionFunction($handler);
+        } else {
+            // The handler is neither, throw an exception
+            throw new \Exception("The provided \$handler is neither callable or an array", 1);
         }
+
+        // List of all the parameters the function accepts that we are also providing
+        $parameters = [];
+
+        // Look at each parameter (function argument) of the handler
+        foreach ($reflection->getParameters() as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+
+            // if we accept a $request, add it to the parameters list
+            if (isset($type) && $type instanceof \ReflectionNamedType && $type->getName() === Request::class) {
+                $parameters[] = $request;
+            }
+            // now, check if the current parameter has a corresponding name in the params array
+            else if (isset($params[$name ?? ''])) {
+                $parameters[] = $params[$name];
+            }
+            // since we're relying on order for the parameters to be inserted, see if the parameter has a default value
+            else if ($param->isDefaultValueAvailable()) {
+                $parameters[] = $param->getDefaultValue();
+            }
+            // if the parameter is nullable, set it to null
+            else if ($param->allowsNull()) {
+                $parameters[] = null;
+            }
+            // if none of the above conditions were met, the route fucked up somehow. Throw an error in that case
+            else {
+                throw new \Exception("The following required \$handler parameter was not provided: $name", 1);
+            }
+        }
+
+        // call the function with the decided parameters
+        $result = (string) call_user_func_array($handler, $parameters);
+
+        // create response object from the result
+        return new Response($result);
     }
 
     public function handle(Request $request)
