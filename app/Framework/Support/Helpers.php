fa19c4b321bad60a20f0363801bda23d70131b0d
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-19 14:04:15 -0300

    refactor: add facade logic to ViewEngine abstraction

--- app/Framework/Support/Helpers.php
+++ app/Framework/Support/Helpers.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Framework\Facades\ViewEngine;
 use App\Framework\View\Engine;
 
 if (!function_exists('view')) {
     function view(string $name, array $params = [])
     {
         try {
-            $str = Engine::render($name, $params);
+            $str = ViewEngine::render($name, $params);
         } catch (\League\Plates\Exception\TemplateNotFound $e) {
             // get message
             $message = $e->getMessage();
 
             // render message
-            $str = Engine::render("view::error", [
+            $str = ViewEngine::render("view::error", [
                 "error" => "Página não encontrada. Detalhes: $message"
             ]);
         }
--- app/Framework/View/Engine.php
+++ app/Framework/View/Engine.php
@@ -4,48 +4,46 @@ namespace App\Framework\View;
 
 class Engine
 {
-    private \League\Plates\Engine $platesEngine;
+    private const EXTENSION = "phtml";
+    public \League\Plates\Engine $engine;
 
+    // singleton
     private static Engine $_instance;
-    private function __construct($extension)
+    private function __construct()
     {
         // declare params
         $defaultPath = realpath(__DIR__ . '/defaults');
         $viewPath = realpath(__DIR__ . '/../../../resources/views');
 
         // create plate engine with folder
-        $this->platesEngine = new \League\Plates\Engine($defaultPath, $extension);
-        $this->platesEngine->addFolder('view', $viewPath, true);
+        $this->engine = new \League\Plates\Engine($defaultPath, self::EXTENSION);
+        $this->engine->addFolder('view', $viewPath, true);
     }
 
-    private static function singleton(): Engine
+    public static function singleton(): Engine
     {
         if (!isset(self::$_instance)) {
-            self::$_instance = new Engine("phtml");
+            self::$_instance = new Engine();
         }
         return self::$_instance;
     }
+    // end singleton
 
-    public static function render(string $name, array $data = [])
+    public function render(string $name, array $data = []): string
     {
-        // support dot syntax for folder pathing
+        // Support dot syntax for folder pathing
         $name = str_replace('.', '/', $name);
 
-        // Render the engine
         try {
-            $result = Engine::get()->render("view::$name", $data);
+            $result = $this->engine->render("view::$name", $data);
         } catch (\League\Plates\Exception\TemplateNotFound $e) {
             $message = $e->getMessage();
-            $result = Engine::get()->render("view::error", [
-                "error" => "Página não encontrada. Detalhes: $message"
+            $result = $this->engine->render("view::error", [
+                "code" => 500,
+                "message" => "Erro ao renderizar página. Detalhes: $message",
             ]);
         }
 
         return $result;
-    }
-
-    public static function get()
-    {
-        return static::singleton()->platesEngine;
     }
 }
