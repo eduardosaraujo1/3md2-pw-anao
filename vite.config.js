145b2fb95856c271f95eb17c30cbcbd90b33e839
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-22 19:41:04 -0300

    feat: vite working minimum

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        host: '0.0.0.0', // docker-compone
+        // host: 'localhost', // windows
+        port: 5173, // forward port in docker compose
+    },
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+        },
+    },
+    plugins: [tailwindcss()],
+});
