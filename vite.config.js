709858ff36ad42b61a59f89a8f0b5d48a7b385b8
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-23 12:15:15 -0300

    fix: vite not reading .env file

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    server: {
-        allowedHosts: [
-            'vite', // for accessing the vite server from PHP
-        ],
-        host: process.env.VITE_HOST || '0.0.0.0',
-        port: process.env.VITE_PORT ?? 5173,
-    },
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: true,
-        rollupOptions: {
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        server: {
+            host: env.VITE_HOST ?? '0.0.0.0',
+            port: env.VITE_PORT ?? 5173,
         },
-    },
-    plugins: [tailwindcss()],
-    publicDir: false,
+        build: {
+            outDir: 'public/build',
+            emptyOutDir: true,
+            manifest: true,
+            rollupOptions: {
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+            },
+        },
+        plugins: [tailwindcss()],
+        publicDir: false,
+    };
 });
