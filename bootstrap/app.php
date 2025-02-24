ce4cd295fc7d72af7142271520907b5cd3119ed5
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-24 11:53:43 -0300

    fix: better error handling on missing composer autoload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,25 @@
 <?php
 
-// Define project root path (relative to bootstrap/app.php)
+// Define project root path
 define('PROJECT_ROOT', dirname(__DIR__));
 
-// Require composer autoload dependencies
-require_once PROJECT_ROOT . '/vendor/autoload.php';
+// Require composer autoload with error checking
+$autoloadPath = PROJECT_ROOT . '/vendor/autoload.php';
+if (file_exists($autoloadPath)) {
+    require_once $autoloadPath;
+} else {
+    exit(
+        <<<HTML
+        <div style="text-align:center">
+            <h1>Arquivo <code>vendor/autoload.php</code> não foi encontrado</h1>
+            <span>Por favor execute <code>composer install</code> ou <code>composer dump-autoload</code> para gera-lo.</span>
+        </div>
+        HTML);
+}
 
-// Require function helpers for application
-require_once PROJECT_ROOT . '/app/Framework/Support/Helpers.php';
-
-// Start app session
+// Start app session (if necessary will be moved to a standalone class)
 session_start();
 
-// Load env
+// Load .env file
 $dotenv = Dotenv\Dotenv::createImmutable(PROJECT_ROOT);
 $dotenv->load();
